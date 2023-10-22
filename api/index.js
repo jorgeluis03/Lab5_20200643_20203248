108886f1c51c70a6e6d59a0b18d2299049279cea
noe <noe.jaraq@gmail.com>
2023-10-22 11:16:31 -0500

    rutas api

---
+++ api/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+const PORT = '8080'
+
+let conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'hr_v2'
+})
+
+conn.connect(err => {
+    if (err) throw err;
+    console.log('Conexion exitosa con database');
+});
+
+// retorna lista de trabajadores de tutor en especifico
+app.get('/tutor/:codigoTutor', (req, res) => {
+    let codigoTutor = req.params.codigoTutor;
+    let query = 'SELECT * FROM employees WHERE employees.manager_id = '+codigoTutor;
+    conn.query(query, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+})
+
+// retorna informacion de trabajador en especifico
+app.get('/tutor/trabajador/:codigoTrabajador', (req, res) => {
+    let codigoTrabajador = req.params.codigoTrabajador;
+    let query = 'SELECT * FROM employees WHERE employees.employee_id = '+codigoTrabajador;
+    conn.query(query, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    })
+})
+
+// creacion de tutoria
+app.post('/tutor/tutorias/:codigoTutor/:codigoTrabajador', (req, res) => {
+    let codigoTutor = req.params.codigoTutor;
+    let codigoTrabajador = req.params.codigoTrabajador;
+    console.log(codigoTutor+" "+codigoTrabajador);
+})
+
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en ',PORT);
+})

fb93dbe8c75b4333d777d16cbb67d62331aa5088
noe <noe.jaraq@gmail.com>
2023-10-23 00:02:47 -0500

    asignacion de tutoria agregado

--- appandroid/app/src/main/java/com/example/lab5_20200643_20203248/services/TutorService.java
+++ appandroid/app/src/main/java/com/example/lab5_20200643_20203248/services/TutorService.java
@@ -2,10 +2,12 @@ package com.example.lab5_20200643_20203248.services;
 
 import com.example.lab5_20200643_20203248.entity.TrabajadorEntity;
 
+import java.util.HashMap;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface TutorService {
@@ -14,4 +16,8 @@ public interface TutorService {
 
     @GET("/tutor/trabajador/{codigo}")
     Call<List<TrabajadorEntity>> getTrabajador(@Path("codigo") int codigo);
+
+    @POST("/tutor/tutorias/{codigoTutor}/{codigoTrabajador}")
+    Call<HashMap<String, String>> postTutoria(@Path("codigoTutor") int codigoTutor,
+                                              @Path("codigoTrabajador") int codigoTrabajador);
 }
--- appandroid/app/src/main/java/com/example/lab5_20200643_20203248/tutor/TutorAsigarTutoria.java
+++ appandroid/app/src/main/java/com/example/lab5_20200643_20203248/tutor/TutorAsigarTutoria.java
@@ -3,21 +3,92 @@ package com.example.lab5_20200643_20203248.tutor;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.lab5_20200643_20203248.R;
+import com.example.lab5_20200643_20203248.databinding.ActivityTutorAsigarTutoriaBinding;
+import com.example.lab5_20200643_20203248.services.TutorService;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TutorAsigarTutoria extends AppCompatActivity {
+    private ActivityTutorAsigarTutoriaBinding binding;
+    private TutorService tutorService;
+    private final String HOST = "192.168.1.9";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tutor_asigar_tutoria);
+        binding = ActivityTutorAsigarTutoriaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         Toolbar toolbar = findViewById(R.id.toolbarTutorAsignarTutorial);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        tutorService = new Retrofit.Builder()
+                .baseUrl("http://"+HOST+":8080")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(TutorService.class);
 
+        binding.buttonAsignarTutor.setOnClickListener(v -> {
+            if (accesoInternet()){
+                String codigoTutor = binding.inputCoidigoTutor.getEditText().getText().toString();
+                String codigoTrabajador = binding.inputCoidigoTrabajador.getEditText().getText().toString();
+                postTutoria(codigoTutor, codigoTrabajador);
+            }
+            else{
+                Toast.makeText(TutorAsigarTutoria.this, "Error: Verifique su conexión con internet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void postTutoria(String codigoTutor, String codigoTrabajador){
+        tutorService.postTutoria(Integer.parseInt(codigoTutor), Integer.parseInt(codigoTrabajador)).enqueue(new Callback<HashMap<String, String>>() {
+            @Override
+            public void onResponse(Call<HashMap<String, String>> call, Response<HashMap<String, String>> response) {
+                if (response.isSuccessful()){
+                    HashMap<String, String> root = response.body();
+                    switch (root.get("msg")){
+                        case "Trabajador inválido":
+
+                            break;
+                        case "Trabajador ocupado":
+
+                            break;
+                        case "ok":
+
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HashMap<String, String>> call, Throwable t) {
+                Log.d("msg-test", "error: "+t.getMessage());
+            }
+        });
+    }
+
+    private boolean accesoInternet(){
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetworkInfo = manager.getActiveNetworkInfo();
+        boolean tieneInternet = activeNetworkInfo != null && activeNetworkInfo.isConnected();
+
+        return tieneInternet;
     }
 }
